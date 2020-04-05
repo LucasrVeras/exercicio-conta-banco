aa6887f859511e5ce677ae39ddc3da0314e7fefb
Lucas Veras <lucas.veras01@gmail.com>
2020-04-04 23:53:16 -0300

    Comentarios no codigo corrigidos

--- src/entities/Conta.java
+++ src/entities/Conta.java
@@ -5,47 +5,48 @@ public class Conta {
 	private int numeroConta;
 	private String nome;
 	private double saldo;
-	
-	//Construtores
-	
+
+	// Construtores
 	public Conta(int conta, String nome) {
 		this.numeroConta = conta;
 		this.nome = nome;
 	}
-	
+
 	public Conta(int conta, String nome, double saldo) {
 		this.numeroConta = conta;
 		this.nome = nome;
 		this.saldo = saldo;
 	}
-	
-	//Métados
-	public void deposito(double total) { //Deposito
-		saldo += total;
-	}
-	
-	public void saque(double total) { //saque
-		saldo -= total + 5.0;
-	}
-	
-	//Getters and Setters 
+
+	// Getters and Setters
 	public int getNumeroConta() {
 		return numeroConta;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public double getDeposito() {
 		return saldo;
 	}
-	
+
+	//Metados
+	public void deposito(double total) { // Deposito
+		saldo += total;
+	}
+
+	public void saque(double total) { // saque
+		saldo -= total + 5.0;
+	}
+
 	@Override
 	public String toString() {
 		return "Conta Numero: " + numeroConta + ", Nome do Titular: " + nome + ", Saldo em Conta: " + saldo;
 	}
-	
-	
+
 }
